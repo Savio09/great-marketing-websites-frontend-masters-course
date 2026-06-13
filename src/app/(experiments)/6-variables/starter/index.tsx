0bd6676814362195993faf34e40a1746fa95a72f
Savio09 <declanfortune5@gmail.com>
2026-06-12 23:03:23 -0700

    got more GAP parctise

--- src/app/(experiments)/6-variables/starter/index.tsx
+++ src/app/(experiments)/6-variables/starter/index.tsx
@@ -3,18 +3,48 @@
 import { cn } from "@/lib/utils";
 import s from "./styles.module.css";
 
+import { useEffect } from "react";
+import { distance } from "@/lib/math";
+
 export default function Page() {
+  useEffect(() => {
+    const controller = new AbortController();
+
+    window.addEventListener(
+      "mousemove",
+      (event) => {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+
+        const centerX = screenWidth / 2;
+        const centerY = screenHeight / 2;
+
+        const d = distance(centerX, centerY, mouseX, mouseY);
+        const maxDist = distance(0, 0, centerX, centerY);
+
+        console.log(d / maxDist);
+      },
+      { signal: controller.signal },
+    );
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
   return (
     <div
       className={cn(
         "w-screen h-screen text-white flex items-center justify-center",
-        s.grid
+        s.grid,
       )}
     >
       <h1
         className={cn(
           "uppercase text-[10vh] leading-none relative",
-          s["title"]
+          s["title"],
         )}
       >
         Variables
--- src/app/(experiments)/7-custom-cursor/starter/index.tsx
+++ src/app/(experiments)/7-custom-cursor/starter/index.tsx
@@ -1,14 +1,37 @@
 "use client";
 
 import s from "./styles.module.css";
+import { useEffect, useRef } from "react";
 
 export default function Page() {
+  const mouseRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const controller = new AbortController();
+
+    window.addEventListener(
+      "mousemove",
+      (event) => {
+        const x = event.clientX;
+        const y = event.clientY;
+
+        if (mouseRef.current) {
+          mouseRef.current.style.setProperty("--x", x.toString() + "px");
+          mouseRef.current.style.setProperty("--y", y.toString() + "px");
+        }
+      },
+      { signal: controller.signal },
+    );
+
+    return () => {
+      controller.abort();
+    };
+  });
   return (
     <div className="w-screen h-screen bg-black text-green-400 flex items-center justify-center">
       <h1 className="uppercase text-[10vh] leading-none relative cursor-default pl-[0.1em] opacity-60 hover:opacity-100">
         Start
       </h1>
-      <div className={s.cursor} />
+      <div ref={mouseRef} className={s.cursor} />
     </div>
   );
 }
