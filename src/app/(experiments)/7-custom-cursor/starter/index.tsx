78a56da95f643782e9efd05a1c08b147b5268fb4
Savio09 <declanfortune5@gmail.com>
2026-06-22 21:49:19 -0700

    continuing animations

--- src/app/(experiments)/7-custom-cursor/starter/index.tsx
+++ src/app/(experiments)/7-custom-cursor/starter/index.tsx
@@ -1,10 +1,67 @@
 "use client";
 
+import { lerp } from "three/src/math/MathUtils.js";
 import s from "./styles.module.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useCallback, useState } from "react";
+import gsap from "gsap";
 
 export default function Page() {
   const mouseRef = useRef<HTMLDivElement>(null);
+
+  const cursorPosRef = useRef({
+    x: 0,
+    y: 0,
+  });
+
+  const cursorTargetRef = useRef({
+    x: 0,
+    y: 0,
+  });
+
+  const [snap, setSnap] = useState<null | {
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  }>(null);
+
+  useEffect(() => {
+    const cb = () => {
+      if (snap) {
+        cursorPosRef.current.x = lerp(cursorPosRef.current.x, snap.x, 0.1);
+        cursorPosRef.current.y = lerp(cursorPosRef.current.y, snap.y, 0.1);
+      } else {
+        cursorPosRef.current.x = lerp(
+          cursorPosRef.current.x,
+          cursorTargetRef.current.x,
+          0.1,
+        );
+        cursorPosRef.current.y = lerp(
+          cursorPosRef.current.y,
+          cursorTargetRef.current.y,
+          0.1,
+        );
+      }
+
+      if (mouseRef.current) {
+        mouseRef.current.style.setProperty(
+          "--x",
+          cursorPosRef.current.x.toString() + "px",
+        );
+        mouseRef.current.style.setProperty(
+          "--y",
+          cursorPosRef.current.y.toString() + "px",
+        );
+      }
+    };
+
+    gsap.ticker.add(cb);
+
+    return () => {
+      gsap.ticker.remove(cb);
+    };
+  }, [snap]);
+
   useEffect(() => {
     const controller = new AbortController();
 
@@ -15,8 +72,10 @@ export default function Page() {
         const y = event.clientY;
 
         if (mouseRef.current) {
-          mouseRef.current.style.setProperty("--x", x.toString() + "px");
-          mouseRef.current.style.setProperty("--y", y.toString() + "px");
+          // mouseRef.current.style.setProperty("--x", x.toString() + "px");
+          // mouseRef.current.style.setProperty("--y", y.toString() + "px");
+          cursorTargetRef.current.x = x;
+          cursorTargetRef.current.y = y;
         }
       },
       { signal: controller.signal },
@@ -26,12 +85,41 @@ export default function Page() {
       controller.abort();
     };
   });
+
+  const onPointerEnter = useCallback((e: PointerEvent) => {
+    const rect = e.currentTarget.getClientRects()[0];
+    console.log(rect);
+    setSnap({
+      x: window.innerWidth / 2,
+      y: window.innerHeight / 2,
+      w: rect.width,
+      h: rect.height,
+    });
+  }, []);
+
+  const onPointerLeave = useCallback(() => {
+    setSnap(null);
+  }, []);
+
   return (
     <div className="w-screen h-screen bg-black text-green-400 flex items-center justify-center">
-      <h1 className="uppercase text-[10vh] leading-none relative cursor-default pl-[0.1em] opacity-60 hover:opacity-100">
+      <h1
+        onPointerEnter={onPointerEnter}
+        onPointerLeave={onPointerLeave}
+        className="uppercase text-[10vh] leading-none relative cursor-default pl-[0.1em] opacity-60 hover:opacity-100"
+      >
         Start
       </h1>
-      <div ref={mouseRef} className={s.cursor} />
+      <div
+        ref={mouseRef}
+        className={s.cursor}
+        style={
+          {
+            "--w": snap ? snap.w + "px" : undefined,
+            "--h": snap ? snap.h + "px" : undefined,
+          } as React.CSSProperties
+        }
+      />
     </div>
   );
 }
--- src/app/(experiments)/8-image-sequence/starter/index.tsx
+++ src/app/(experiments)/8-image-sequence/starter/index.tsx
@@ -3,12 +3,16 @@
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 import gsap from "gsap";
-import { SplitText } from "gsap/all";
+import { SplitText, ScrollTrigger } from "gsap/all";
+import { useEffect } from "react";
+import { fitContent, remap } from "@/lib/math";
 
 gsap.registerPlugin(SplitText);
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Page() {
   const containerRef = useRef<HTMLDivElement>(null);
+  const progressRef = useRef(0);
 
   useGSAP(
     () => {
@@ -26,19 +30,33 @@ export default function Page() {
         duration: 0.5,
         ease: "circ.out",
       });
+
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top top",
+          end: "bottom bottom",
+          scrub: true,
+        },
+      });
+
+      tl.to(progressRef, {
+        current: 1,
+      });
     },
-    { scope: containerRef }
+    { scope: containerRef },
   );
 
   return (
     <div ref={containerRef} className="h-[400vh]">
+      <ScrollSequence progress={progressRef} />
       <div className="relative w-full overflow-clip">
-        <section className="title h-screen relative w-full">
+        <section className="title h-screen fixed w-full">
           <h1 className="uppercase absolute text-[8vw] w-full text-center -bottom-[0.1em] leading-none right-[0.05em] tracking-widest text-transparent">
             Perseverance
           </h1>
         </section>
-        <section className="cameras relative h-screen w-full top-0 left-0 opacity-100">
+        <section className="cameras fixed h-screen w-full top-0 left-0 opacity-100">
           <div className="absolute top-1/2 -translate-y-1/2 right-10 max-w-full w-md text-white">
             <h2 className="text-6xl mb-2">Cameras</h2>
             <p className="text-balance">
@@ -49,7 +67,7 @@ export default function Page() {
             </p>
           </div>
         </section>
-        <section className="wheels relative h-screen w-full opacity-100">
+        <section className="wheels fixed h-screen w-full opacity-100">
           <div className="absolute bottom-10 left-16 max-w-full w-md text-white">
             <h2 className="text-6xl mb-2">Wheels</h2>
             <p className="text-balance">
@@ -61,4 +79,57 @@ export default function Page() {
       </div>
     </div>
   );
+}
+
+function ScrollSequence({ progress }: { progress: React.RefObject<number> }) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    function resize() {
+      if (!canvas) return;
+      canvas.width = canvas.offsetWidth;
+      canvas.height = canvas.offsetHeight;
+    }
+    resize();
+    window.addEventListener("resize", resize);
+
+    const images: Record<number, HTMLImageElement> = {};
+
+    for (let i = 0; i < 300; i++) {
+      const image = new Image();
+      image.src = `/sequence/${(i + 1).toString().padStart(4, "0")}.webp`;
+      image.onload = () => {
+        images[i + 1] = image;
+      };
+    }
+
+    const cb = gsap.ticker.add(() => {
+      if (!canvas || !ctx) return;
+      let frameNumber = Math.round(remap(progress.current, 0, 1, 1, 300));
+      let image = images[frameNumber];
+
+      if (!image) return;
+      const { x, y, width, height } = fitContent(
+        canvas.width,
+        canvas.height,
+        image.width,
+        image.height,
+      );
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(image, x, y, width, height);
+    });
+
+    return () => {
+      window.removeEventListener("resize", resize);
+      gsap.ticker.remove(cb);
+    };
+  }, []);
+
+  return <canvas ref={canvasRef} className="fixed inset-0 w-full h-full" />;
 }
